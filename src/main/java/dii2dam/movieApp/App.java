8f3749ce0ae09b62be6469b1c046857f5076d32d
MorsaAstuta <naujzenitramaicitsuj@gmail.com>
2023-11-28 10:06:57 +0100

    signLogLayout

--- src/main/java/dii2dam/movieApp/App.java
+++ src/main/java/dii2dam/movieApp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 808, 585);
+        scene = new Scene(loadFXML("secondary"), 808, 585);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/dii2dam/movieApp/PrimaryController.java
+++ src/main/java/dii2dam/movieApp/PrimaryController.java
@@ -1,12 +1,19 @@
 package dii2dam.movieApp;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  void initialize() {
+
+  }
 }
