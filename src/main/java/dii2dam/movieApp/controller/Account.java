b60296abdc2c72a6e5a941a1cea86c8730c81e57
AlbertoEP123 <116571236+AlbertoEP123@users.noreply.github.com>
2024-01-16 10:44:36 +0100

    Acc

--- src/main/java/dii2dam/movieApp/controller/Account.java
+++ src/main/java/dii2dam/movieApp/controller/Account.java
@@ -1,5 +1,19 @@
 package dii2dam.movieApp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Account {
+	 @FXML
+	    private Label idEmail;
+
+	    @FXML
+	    private Label idLastConnect;
+
+	    @FXML
+	    private Label idUsername;
+
+	    @FXML
+	    private Label ifFullUsername;
 
 }
