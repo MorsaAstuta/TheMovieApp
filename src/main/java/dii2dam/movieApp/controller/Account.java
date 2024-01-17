3d2f691f7a4efe1de00f16115c98aa8497865da7
MorsaAstuta <naujzenitramaicitsuj@gmail.com>
2024-01-17 07:59:40 +0100

    23-01-17_08:00

--- src/main/java/dii2dam/movieApp/controller/Account.java
+++ src/main/java/dii2dam/movieApp/controller/Account.java
@@ -2,18 +2,52 @@ package dii2dam.movieApp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class Account {
-	 @FXML
-	    private Label idEmail;
 
-	    @FXML
-	    private Label idLastConnect;
+  @FXML
+  private Pane btnAdd;
 
-	    @FXML
-	    private Label idUsername;
+  @FXML
+  private Pane btnConfig;
 
-	    @FXML
-	    private Label ifFullUsername;
+  @FXML
+  private Pane btnMyList;
 
+  @FXML
+  private Pane btnSignOut;
+
+  @FXML
+  private Label idEmail;
+
+  @FXML
+  private Label idLastConnect;
+
+  @FXML
+  private Label idUsername;
+
+  @FXML
+  private Label ifFullUsername;
+
+  @FXML
+  void addMovie(MouseEvent event) {
+
+  }
+
+  @FXML
+  void myList(MouseEvent event) {
+
+  }
+
+  @FXML
+  void pageConfig(MouseEvent event) {
+
+  }
+
+  @FXML
+  void signOut(MouseEvent event) {
+
+  }
 }
