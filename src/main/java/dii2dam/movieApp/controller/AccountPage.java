6fd591f7dfe5e3dd5e03b238ede3504302e29e3d
AlbertoEP123 <116571236+AlbertoEP123@users.noreply.github.com>
2024-02-15 14:39:04 +0100

    Acc/ConfAcc

--- src/main/java/dii2dam/movieApp/controller/AccountPage.java
+++ src/main/java/dii2dam/movieApp/controller/AccountPage.java
@@ -6,6 +6,8 @@ import dii2dam.movieApp.App;
 import dii2dam.movieApp.utils.Manager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
@@ -33,15 +35,33 @@ public class AccountPage {
 	private Label idUsername;
 
 	@FXML
-	private Label ifFullUsername;
+	private ImageView idImgPoster1;
+
+    @FXML
+    private ImageView idImgPoster2;
+
+    @FXML
+    private ImageView idImgPoster3;
+
+    @FXML
+    private ImageView idImgPoster4;
 
 	public void initialize() {
-		if(Manager.user!=null) {
+		if (Manager.user != null) {
 			idUsername.setText(Manager.user.getUsername());
 			idEmail.setText(Manager.user.getMail());
 			idLastConnect.setText(Manager.user.getRegister_date());
 		}
-		
+		if (Manager.movie != null) {
+			String poster = Manager.movie.getPoster_path();
+			String urlPoster = "https://image.tmdb.org/t/p/w500" + poster;
+			Image image = new Image(urlPoster);
+			idImgPoster1.setImage(image);
+			idImgPoster2.setImage(image);
+			idImgPoster3.setImage(image);
+			idImgPoster4.setImage(image);
+		}
+
 	}
 
 	@FXML
